6b7206b0f66d6aa825f3dab39863cf41e49e7065
Minya Yao <ymycissy@gmail.com>
2018-10-14 01:20:15 -0700

    Link Reviews with Restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_071901) do
+ActiveRecord::Schema.define(version: 2018_10_14_075802) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_071901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
