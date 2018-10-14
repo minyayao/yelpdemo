972cee4b77a649b467568e6145d2a45b5e242782
Minya Yao <ymycissy@gmail.com>
2018-10-14 00:27:06 -0700

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_062348) do
+ActiveRecord::Schema.define(version: 2018_10_14_071901) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_062348) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
